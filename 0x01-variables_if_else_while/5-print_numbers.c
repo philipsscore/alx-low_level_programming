d593ea1c4c5ec87ffbdf4252cfcbbfefb8c5fa0b
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-09 14:19:32 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+
+	   int ch;
+
+
+	     for(ch = 1 ; ch < '10' ; ch++)
+	    
+	     {
+		     putchar(ch);
+			     }
+	     return(0);
+
+}
