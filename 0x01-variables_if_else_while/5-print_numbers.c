292990d3b574454ab8dddab43e7801857d04742a
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-09 14:58:37 -0700

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 0 ; ch < 10 ; ch++)
-	{
-	printf("%d", ch);
+	for (ch = 0 ; ch < 10 ; ch++){
+	
+		printf("%d", ch);
 	}
        	printf("\n");
 	return (0);
