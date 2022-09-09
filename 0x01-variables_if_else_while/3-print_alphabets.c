aa107342297b0f2024635913c484e59b8121b14d
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-09 12:38:29 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+
+	char num;
+
+	num = 'a';
+
+	while (num <= 'z')
+
+	{
+
+		putchar(num);
+
+		num++
+
+	}
+
+
+
+	char num;
+
+	num = 'A';
+
+	while (num <= 'Z')
+
+	{
+
+		putchar(num);
+
+		num++
+
+	}
+
+
+
+	putchar('\n');
+
+	return (0);
+
+}
