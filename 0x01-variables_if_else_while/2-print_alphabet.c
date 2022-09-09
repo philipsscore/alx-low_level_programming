0558d27038eeb3f83d80f31a1d2404c23dc64cab
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-09 10:58:44 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * */
+
+int main(void)
+
+{ 
+
+	char ch;
+
+
+	  for(ch = 'a' ; ch <= 'z' ; ch++) 
+
+	  {
+
+		        putchar(ch);
+
+			   }
+
+	     
+
+	     return(0);
+
+}
