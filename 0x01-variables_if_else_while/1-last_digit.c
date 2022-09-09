26be15aee3910f02826112ef09e638fcd3339531
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-09 09:48:29 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,44 +14,25 @@ int main(void)
 
 {
 
-		int n,s;
+	int n, s;
+	srand(time(0));
 
+	n = rand() - RAND_MAX / 2;
 
+	s = n % 10;
 
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-					s = n % 10
-
-						        if (s > 5)
-
-							{
-
-								printf("Last digit of %d is %d aand is greater than 5\n", n, s);
-
-
-
-							}
-
-					if (s == 0)
-
-					{
-
-						printf("Last digit of %d is %d and is 0\n", n, s);
-
-					}
-
-
-
-					if (s < 0)
-
-					{
-
-						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
-
-					}
-
-						return (0);
+	if (s > 5)
+	{
+	printf("Last digit of %d is %d aand is greater than 5\n", n, s);
+	}
+	if (s == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, s);
+	}
+	if (s < 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
+	}
+	return (0);
 
 }
