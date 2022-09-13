1ca9fe008e45ccd6ebaecdcf07f194d098a210c3
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-13 12:50:04 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
+* pint_alphabet function to print c  
 * main - Entry point
 * Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
 	char alp = 'a';
-	
 	while (alp <= 'z')
 	{
 		_putchar(alp);
