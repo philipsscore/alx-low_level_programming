87899697ebb472bf51420e731eed4a1ce9891515
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-13 12:42:21 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,12 @@
 
 void print_alphabet(void)
 {
-
-	    char alp = 'a';
-	    
-	    while (alp <= 'z')
-	    {
-		    _putchar(alp);
-		    alp++;
-	    }
-	    _putchar('\n');
+	char alp = 'a';
+	
+	while (alp <= 'z')
+	{
+		_putchar(alp);
+		alp++;
+	}
+	_putchar('\n');
 }
