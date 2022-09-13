a6f9d3839250abff092d53ae0ab7b355db345968
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-13 09:59:20 -0700

    02f

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,31 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 /**
 * main - Entry point
 * Return: Always 0 (Success)
 */
 void print_alphabet(void)
-
 {
 
 	    char alp = 'a';
-
-
-
-	       while (alp <= 'z')
-
-		           {
-
-				           putchar (alp);
-
-					           alp++;
-
-
-
-						      }
-
-	           putchar ('\n');
-
-
-
+	    
+	    while (alp <= 'z')
+	    {
+		    putchar (alp);
+		    alp++;
+	    }
+	    putchar ('\n');
 }
