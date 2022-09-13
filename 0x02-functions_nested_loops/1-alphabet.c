0bf70ae65dc499fecd499253444594e30317438f
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-13 09:19:03 -0700

    02f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+	char num;
+	
+	num = 'a';
+	while (num <= 'z')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+
+}
