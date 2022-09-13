0e4856d80cbfce84c72f0e5fce46d326716039af
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-13 13:10:11 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
-* return : 0
-*print_alphabet - function to print c  
-* main - Entry point
+* print_alphabet - function to print c  
+* 
 * Return: Always 0 (Success)
 */
 void print_alphabet(void)
