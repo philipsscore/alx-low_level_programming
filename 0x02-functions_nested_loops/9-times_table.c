aed77393b3604a70a2a2820dfc7b62c5a685c250
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-13 17:46:10 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,41 +1,75 @@
 #include "main.h"
 
 /**
-* times_table - Print 9 times tabl
+* times_table - Print 9 times table
 * Return: Void.
 */
 
+
 void times_table(void)
+
 {
-	int x, y, multiplied_value;
 
-	for (y = 0; y < 10; y++)
-	{
-		for (x = 0; x < 10; x++)
-		{ 
-			multiplied_value = x * y;
+	    int x, y, multiplied_value;
 
-			if (x == 0)
-		       	{
-				_putchar(multiplied_value + '0');
-			}
-			else if (multiplied_value >= 10)
-			{
-			       	_putchar(' ');
-				_putchar(multiplied_value / 10 + '0');
-				_putchar(multiplied_value % 10 + '0'); 
-			}
-		       	else
-			{
-			       	_putchar(' ');
-				_putchar(' ');
-				_putchar(multiplied_value + '0');
-			}
-			if (x != 9)
-		       	{
-				_putchar(',');
-		       	}
-	       }
-		_putchar('\n');
-       	}
+
+
+
+
+	       for (y = 0; y < 10; y++)
+
+		           {
+
+				           for (x = 0; x < 10; x++)
+
+						           {
+
+								               multiplied_value = x * y;
+
+									                   if (x == 0)
+
+												               {
+
+														                       _putchar(multiplied_value + '0');
+
+																                   }
+
+											               else if (multiplied_value >= 10)
+
+													                   {
+
+																                   _putchar(' ');
+
+																		                   _putchar(multiplied_value / 10 + '0');
+
+																				                   _putchar(multiplied_value % 10 + '0');
+
+																						               }
+
+												                   else
+
+															               {
+
+																	                       _putchar(' ');
+
+																			                       _putchar(' ');
+
+																					                       _putchar(multiplied_value + '0');
+
+																							                   }
+
+														               if (x != 9)
+
+																                   {
+
+																			                   _putchar(',');
+
+																					               }
+
+															               }
+
+					           _putchar('\n');
+
+						       }
+
 }
