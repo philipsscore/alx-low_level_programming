cd7c067ef98a18527ec1c4b3a4522cf4b9d73e13
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-13 19:17:11 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,13 @@
 */
 void times_table(void)
 {
-    int x, y, multiplied_value;
-    for (y = 0; y < 10; y++)
-    {
-        for (x = 0; x < 10; x++)
-        {
-            multiplied_value = x * y;
-	    if (x == 0)
+       	int x, y, multiplied_value;
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			multiplied_value = x * y;
+				if (x == 0)
 	    {
 		    _putchar(multiplied_value + '0');
 	    }
