c27e65ea4ef36d65a26e989ed4b58cb37a73de3d
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-13 19:13:16 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,27 +11,27 @@ void times_table(void)
         for (x = 0; x < 10; x++)
         {
             multiplied_value = x * y;
-            if (x == 0)
-            {
-                _putchar(multiplied_value + '0');
-            }
-            else if (multiplied_value >= 10)
-            {
-                _putchar(' ');
-                _putchar(multiplied_value / 10 + '0');
-                _putchar(multiplied_value % 10 + '0');
-            }
-            else
-            {
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(multiplied_value + '0');
-            }
-            if (x != 9)
-            {
-                _putchar(',');
-            }
-        }
-        _putchar('\n');
+	    if (x == 0)
+	    {
+		    _putchar(multiplied_value + '0');
+	    }
+	    else if (multiplied_value >= 10)
+	    {
+		    _putchar(' ');
+		    _putchar(multiplied_value / 10 + '0');
+		    _putchar(multiplied_value % 10 + '0');
+	    }
+	    else
+	    {
+		    _putchar(' ');
+		_putchar(' ');
+		_putchar(multiplied_value + '0');
+	    }
+	    if (x != 9)
+	    {
+		    _putchar(',');
+	    }
+	}
+	_putchar('\n');
     }
 }
