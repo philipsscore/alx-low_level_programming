34945c588bbfa15ca1910b24ee682da8f8a3119b
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-13 19:01:10 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@
 void print_to_98(int n)
 {
 	int i = 0;
-	
+
 	if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
@@ -22,7 +22,7 @@ void print_to_98(int n)
 			}
 			printf("%d", i);
 		}
-       	}
+	}
 	else
        	{
 		for (i = n; i <= 98 ; i++)
