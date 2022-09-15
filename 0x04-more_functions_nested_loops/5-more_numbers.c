6ba864f33b7f27eddbed91a148b97e2d66a8e259
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-15 15:08:54 -0700

    0x045

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,17 @@ void more_number(void)
 	int i, j;
 
 	i = 0;
+
 	while (i < 10)
 	{
 		j = 0;
+		
 		while (j < 15)
 		{
 			if (j >= 10)
+			{
 				_putchar('0' + j / 10);
+			}
 			_putchar('0' + j % 10);
 			j++
 		}
