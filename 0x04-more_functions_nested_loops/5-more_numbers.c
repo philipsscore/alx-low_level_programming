12bf0e16c545cc550707fca1abf4ee4f702117d1
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-15 15:27:24 -0700

    055

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,31 +1,29 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 * more_numbers - Print numbers between 0 to 14 incl.
 *
 * Return: Void.
 */
-
-void more_number(void)
+void more_numbers(void)
 {
-	int i, j;
+	int i;
+	int x;
 
-	i = 0;
-
-	while (i < 10)
+	for (x = 0; x < 10; x++)
 	{
-		j = 0;
-		
-		while (j < 15)
+		i = 0;
+
+		while (i <= 14)
 		{
-			if (j >= 10)
+			if (i >= 10)
 			{
-				_putchar('0' + j / 10);
+				_putchar(i / 10 + '0');
 			}
-			_putchar('0' + j % 10);
-			j++
+			_putchar(i % 10 + '0');
+			i++
 		}
 		_putchar('\n');
-		i++
 	}
 }
