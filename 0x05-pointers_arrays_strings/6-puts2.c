a8a080419331f7b665d8ef05457fd79d7c55c030
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-19 16:03:12 -0700

    oo5

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,8 +8,17 @@
 void puts2(char *str)
 {
 	int i;
-	for (i = 0; str[i] != '\0'; i++)
-		if (!(i % 2))
-			_putchar(str[i];
-	_putchar(10);
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
 }
+	
+
