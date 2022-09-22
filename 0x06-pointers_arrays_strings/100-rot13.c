bc26f0e60ea578515e4adaee1ec627c775d66d05
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-22 11:01:32 -0700

    06100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,6 +14,8 @@ char *rot13(char *s)
 	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z'};
 	char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M'};
 
+
+
 	for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
 	{
 		for (rotation = 0; rotation < 53; rotation++)
@@ -21,6 +23,7 @@ char *rot13(char *s)
 			if (r1[rotation] == s[stringCount])
 			{
 				 s[stringCount] = r2[rotation];
+
 				 break;
 			}
 		}
