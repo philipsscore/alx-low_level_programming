55f76b728b7596c8796bfd62228eb055b92f76d7
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-26 13:28:42 -0700

    0x075

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,21 @@
 */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	int i;
+	int sum1, sum2;
 
-	for (i = 0; i < size; i++)
+
+	sum1 = 0;
+	sum2 = 0;
+
+	i = 0;
+
+	while (i < size)
 	{
-		sum1 += a[i];
-		sum2 += a[size - i - 1];
-		a += size;
+		sum1 = sum1 + *(a + i * size + i);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
+
+		i++;
 	}
-	printf("%d, ", sum1);
-	printf("%d\n", sum2);
+	printf("%i, %i\n", sum1, sum2);
 }
