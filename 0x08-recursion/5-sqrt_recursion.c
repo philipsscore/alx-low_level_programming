1adb72b1f7d9e4b322ae88bf7631917c383ba2cb
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-28 15:04:29 -0700

    0x0805

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,7 @@
 * @n: the number to deal with.
 *
 * Return: square root of n.
-*/ 
+*/
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -20,7 +20,6 @@ int _sqrt_recursion(int n)
 *
 * Return: the natural sqrt of n
 */
-
 int natural_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
