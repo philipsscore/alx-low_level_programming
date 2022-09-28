63324ddf7bda39a46b03c659ec2015bc6b0267fe
Luke Ihuoma <philipsscore@yahoo.com>
2022-09-28 14:04:20 -0700

    0x0802

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
-* _print_rev_recursion - prints a string in reverse.
-* @s: the string to operate on.
+* _strlen_recursion - returns the length of a string
+* @s: the string whose length is to be calculated.
 *
-* Return: nothing
+* Return: the length of s.
 */
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
