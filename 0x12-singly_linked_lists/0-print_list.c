4a85937453b18c9b9da0f46c83e37bee774fa1cb
Luke Ihuoma <philipsscore@yahoo.com>
2022-10-20 17:18:57 +0100

    0x12

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
-* print_list - prints the contents of a linked list
-* @h: the given linked list
-*
-* Return: number of nodes
-*/
+ * print_list - prints the contents of a linked list
+ * @h: the given linked list
+ *
+ * Return: number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	int num_nodes = 0;
@@ -24,3 +24,4 @@ size_t print_list(const list_t *h)
 
 	return (num_nodes);
 }
+
